eaf453132003d151558f791a0171fe4fb5300072
karim emad <karimkarim20444@gmail.com>
2025-02-25 01:39:57 -0800

    DB

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        // await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://karimkarim20444:SYCpX41Qql4hXoCt@collections.zekp5.mongodb.net/products?retryWrites=true&w=majority&appName=collections");
         console.log("Successfully connected to MongoDB 👍");
     } catch (error) {
         console.error(`Error: ${error.message}`);
