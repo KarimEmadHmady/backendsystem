70e3e1857ff93ede4aa5fbdbbca01f00826b9cb0
karimemad <karimkarim20444@gmail.com>
2025-02-26 15:23:27 +0200

    2/26 2

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.use(
-  cors({
-    origin: "https://system-v1.netlify.app",  
-    credentials: true,  
-  })
-);
+app.use(cors({
+  origin: "https://system-v1.netlify.app",
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true
+}));
 
 // Routes
 app.use("/api/users", userRoutes);
