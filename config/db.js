12afcf6712e6a2efd2f15b6ec5bc2398744f6218
karimemad <karimkarim20444@gmail.com>
2025-02-26 15:43:38 +0200

    2/26 2

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect(process.env.MONGO_URL);
-        await mongoose.connect("mongodb+srv://karimkarim20444:SYCpX41Qql4hXoCt@collections.zekp5.mongodb.net/products?retryWrites=true&w=majority&appName=collections");
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Successfully connected to MongoDB 👍");
     } catch (error) {
         console.error(`Error: ${error.message}`);
