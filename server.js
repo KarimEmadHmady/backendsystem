ca4005dad0106c592110cfa6b2e8aea5927d31b4
karimemad <karimkarim20444@gmail.com>
2025-02-26 15:33:50 +0200

    2/26 2

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: "https://system-v1.netlify.app",
+  origin: "*",
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true
