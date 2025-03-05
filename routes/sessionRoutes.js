22ee41a4923664c75c853905e4222119b316e0ac
karimemad <karimkarim20444@gmail.com>
2025-03-05 14:40:00 +0200

    3/5

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -13,4 +13,14 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.delete("/clear", async (req, res) => {
+  try {
+    await Session.deleteMany({});
+    res.json({ message: "All sessions have been cleared successfully!" });
+  } catch (error) {
+    console.error("🔴 Error deleting sessions:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 export default router;
